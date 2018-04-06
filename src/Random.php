2ce910cb60b23d8370725b20223ca583f3b5e35e
Sergejs Zablockis <zablose@gmail.com>
2018-04-06 14:52:49 +0100

    Method Random::string() is in use. Test added.

--- src/Random.php
+++ src/Random.php
@@ -8,6 +8,24 @@ class Random
     const CHARACTERS = '0123456789abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ';
 
     /**
+     * @param int    $length
+     * @param string $characters
+     *
+     * @return string
+     */
+    public static function string($length, $characters = self::CHARACTERS)
+    {
+        $string = '';
+
+        for ($i = 0; $i < $length; $i++)
+        {
+            $string .= self::char($characters);
+        }
+
+        return $string;
+    }
+
+    /**
      * @param string $characters
      *
      * @return string
