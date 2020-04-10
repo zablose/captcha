543c84008ee599a903f04f73dccbe4ca1211907d
Sergejs Zablockis <zablose@gmail.com>
2020-04-10 15:42:25 +0100

    More unit tests for check method.

--- laravel/tests/Unit/CaptchaTest.php
+++ laravel/tests/Unit/CaptchaTest.php
@@ -7,17 +7,44 @@ use Zablose\Captcha\Captcha;
 class CaptchaTest extends UnitTestCase
 {
     /** @test */
-    public function is_resizeable()
+    public function resizeable()
     {
         $this->assertCaptcha(['width' => 200, 'height' => 40]);
     }
 
     /** @test */
-    public function is_checkable()
+    public function checkable()
     {
         $captcha = new Captcha();
 
         $this->assertTrue(Captcha::check($captcha->sensitive(), $captcha->txt(), $captcha->hash()));
+    }
+
+    /** @test */
+    public function check_fails_if_text_does_not_match()
+    {
+        $captcha = new Captcha();
+
+        $this->assertFalse(Captcha::check($captcha->sensitive(), $captcha->txt().'abc', $captcha->hash()));
+    }
+
+    /** @test */
+    public function check_fails_if_hash_does_not_match()
+    {
+        $captcha = new Captcha();
+
+        $this->assertFalse(Captcha::check($captcha->sensitive(), $captcha->txt(), $captcha->hash().'abc'));
+    }
+
+    /** @test */
+    public function sensitive_check_fails_if_text_case_does_not_match()
+    {
+        $captcha = new Captcha([
+            'characters' => 'abcdef',
+            'sensitive' => true,
+        ]);
+
+        $this->assertFalse(Captcha::check($captcha->sensitive(), strtoupper($captcha->txt()), $captcha->hash()));
     }
 
     /** @test */
@@ -27,7 +54,7 @@ class CaptchaTest extends UnitTestCase
     }
 
     /** @test */
-    public function is_configurable()
+    public function configurable()
     {
         $this->assertCaptcha([
             'contrast' => 50,
