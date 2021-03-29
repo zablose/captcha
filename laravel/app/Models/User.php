699f1151e6d8c40790b71190888c9496c6b89a00
Sergejs Zablockis <zablose@gmail.com>
2021-03-29 14:39:38 +0100

    PHP 8 support update.

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -39,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected static function newFactory(): UserFactory
+    {
+        return new UserFactory();
+    }
 }
